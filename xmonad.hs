51cbd6405ff26ede04956f5cb0466ee2a208e00d
sballert <sballert@posteo.de>
2019-07-22 16:38:23 +0200

    Refactor to an explicit xmobar call

--- xmonad.hs
+++ xmonad.hs
@@ -1,16 +1,23 @@
 import XMonad
-import XMonad.Hooks.DynamicLog (xmobar)
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, ppOutput)
+import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks, docksEventHook)
 import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe, hPutStrLn)
 
 main = do
-  xmonad =<< xmobar myConfig
+  xmobar <- spawnPipe "xmobar"
+  xmonad $ myConfig xmobar
 
-myConfig = def
+myConfig xmobar = def
   { modMask = mod4Mask
   , terminal = "st"
   , borderWidth = 1
   , normalBorderColor = "#3c3836"
   , focusedBorderColor = "#665c54"
+  , logHook = dynamicLogWithPP $ myLogHook xmobar
+  , layoutHook = avoidStruts $ layoutHook defaultConfig
+  , manageHook = manageDocks <+> manageHook defaultConfig
+  , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
   }
   `additionalKeys`
   [ ((mod4Mask, xK_Return), spawn "rofi -show drun")
@@ -19,3 +26,7 @@ myConfig = def
   , ((0, 0x1008ff11), spawn "pulseaudio-ctl down")
   , ((0, 0x1008ff12), spawn "pulseaudio-ctl mute")
   ]
+
+myLogHook xmobar = def
+  { ppOutput = hPutStrLn xmobar
+  }
