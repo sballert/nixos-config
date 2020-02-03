b4047cd82e1d46eb74441bb300a2cd105f59ec98
sballert <sballert@posteo.de>
2020-02-03 21:21:25 +0100

    Move xmobarrc to config subdirectory

--- home.nix
+++ home.nix
@@ -38,8 +38,8 @@ in {
       ".gnupg/sshcontrol".text = ''
         447910F828DF001601E7FAECF768DFA93DF87136
       '';
-      ".config/xmobar/xmobarrc".text = builtins.readFile ./xmobarrc;
       ".emacs.d/init.el".text = readConfig "init.el";
+      ".config/xmobar/xmobarrc".text = readConfig "xmobarrc";
       ".ghci".text = builtins.readFile ./ghci;
     };
   };
