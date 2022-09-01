b04732aa4ea29d03d9529f46bc5c5ffd2e5f6b72
sballert <sballert@posteo.de>
2022-09-01 15:40:09 +0200

    Fix onlykey rules url

--- packages/pkgs/onlykey-udev/default.nix
+++ packages/pkgs/onlykey-udev/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl }: let
 
   file = fetchurl {
-    url = "https://raw.githubusercontent.com/trustcrypto/trustcrypto.github.io/master/49-onlykey.rules";
+    url = "https://raw.githubusercontent.com/trustcrypto/trustcrypto.github.io/pages/49-onlykey.rules";
     sha512 = "2s99masblmw1lb38dmjki6nka028f32w9z9ix0xi9k7bk6a1hcdfmfxsmgf9fha8cdpvnsn14lshjwjzibnph23d3zsysivcp3cr0y2";
   };
 
